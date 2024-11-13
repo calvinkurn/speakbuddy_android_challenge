391cf94cf7f16982d06c17ca72c997123a7c93e6
calvin <calvin.kurn@gmail.com>
2024-11-13 09:37:58 +0700

    add favorite page (no feature yet)

---
+++ app/src/main/java/jp/speakbuddy/factsearcher/ui/view/FavoriteActivity.kt
@@ -0,0 +1,39 @@
+package jp.speakbuddy.factsearcher.ui.view
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.lifecycleScope
+import dagger.hilt.android.AndroidEntryPoint
+import jp.speakbuddy.factsearcher.ui.viewmodel.FactActivityViewModel
+import jp.speakbuddy.factsearcher.ui.theme.FactTheme
+import jp.speakbuddy.factsearcher.ui.viewmodel.FavoriteActivityViewModel
+
+@AndroidEntryPoint
+class FavoriteActivity : ComponentActivity() {
+    private val viewModel: FavoriteActivityViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FactTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+
+        observe()
+    }
+
+    private fun observe() {
+    }
+}
---
+++ app/src/main/java/jp/speakbuddy/factsearcher/ui/viewmodel/FavoriteActivityViewModel.kt
@@ -0,0 +1,12 @@
+package jp.speakbuddy.factsearcher.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class FavoriteActivityViewModel @Inject constructor(
+
+): ViewModel() {
+
+}
