140ab6866abbc9eccb723147972036980b7838e3
calvin <calvin.kurn@gmail.com>
2024-11-19 09:06:28 +0700

    add missing preview for Error dialog widget

--- app/src/main/java/jp/speakbuddy/factsearcher/ui/widget/ErrorPopup.kt
+++ app/src/main/java/jp/speakbuddy/factsearcher/ui/widget/ErrorPopup.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import jp.speakbuddy.factsearcher.R
@@ -68,4 +69,13 @@ fun ErrorPopup(
             }
         }
     }
+}
+
+@Preview
+@Composable
+private fun ErrorPopupPreview() {
+    ErrorPopup(
+        message = "Preview Title",
+        onDismiss = {}
+    )
 }
